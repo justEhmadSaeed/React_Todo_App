9ff767d644bf7c7035979586d53f4439a28e1860
Ehmad <justehmadsaeed@gmail.com>
2021-05-10 22:52:29 +0500

    📦 NEW: Get, Set, Delete APIs

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,11 @@ require('dotenv').config();
 
 const app = express();
 // Routes
-// const menuRouters = require('./src/routes/menu');
-// const restRouters = require('./src/routes/restaurant');
+const listRoutes = require('./src/routes/list');
+
 app.use(express.json());
 app.use(cors());
-// app.use('/api/menu', menuRouters);
-// app.use('/api/rest', restRouters);
+app.use('/api/list', listRoutes);
 
 const port = process.env.PORT || 8000;
 
---
+++ backend/src/routes/list.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { database } = require('../firebase');
+
+// Post Todo List
+router.post('/set', (req, res) => {
+	const { uid, title, todoArray } = req.body;
+	if (uid && title && todoArray.length) {
+		// convert todoList into a text, isComplete Object array
+		const todoObjects = todoArray.map((todo) => {
+			return { text: todo, isComplete: false };
+		});
+		const todoListRef = database.ref(`users/${uid}/TodoLists`).push();
+		todoListRef.set(
+			{
+				title,
+				todoArray: { ...Object(todoObjects) },
+			},
+			(error) => {
+				if (error) return res.status(400).json(error);
+				res.status(200).json({ message: 'Success' });
+			}
+		);
+	} else
+		res.status(400).json({ error: 'Required Fields are missing' });
+});
+// Get all todos
+router.post('/get', (req, res) => {
+	const { uid } = req.body;
+	if (uid) {
+		database.ref(`users/${uid}/TodoLists`).once('value', (snapshot) => {
+			if (snapshot.exists())
+				return res.status(200).json(snapshot.val());
+			else res.status(400).json({ error: 'Data Not Found!' });
+		});
+	} else
+		res.status(400).json({ error: 'Required Fields are missing' });
+});
+// Delete a Todo List
+router.delete('/delete', (req, res) => {
+	const { uid, todoList } = req.body;
+	if (uid && todoList)
+		database
+			.ref(`users/${uid}/TodoLists/${todoList}`)
+			.remove()
+			.then(() => res.status(200).json({ message: 'Success' }))
+			.catch((err) => res.status(400).json(error));
+	else res.status(400).json({ error: 'Required Fields are missing' });
+});
+
+module.exports = router;
